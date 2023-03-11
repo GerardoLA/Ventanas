f52978420a2b936f1f63abcbd953a7df5343a152
GerardoLA <gerardolarrinaga@gmail.com>
2023-03-11 19:25:18 +0100

    pruebatok

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -61,7 +61,7 @@ public class Cliente {
 
 	@Override
 	public String toString() {
-		return "Cliente dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", direccion=" + direccion
+		return "Cliente dni=" + dni  + ", nombre=" + nombre + ", apellidos=" + apellidos + ", direccion=" + direccion
 				+ ", localidad=" + localidad;
 	}
 
